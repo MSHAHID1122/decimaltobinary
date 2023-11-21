0bf744d3dd3d56af1938d24349705b4174f2932f
MSHAHID112 <101447290+MSHAHID112@users.noreply.github.com>
2023-11-21 21:57:08 +0500

    Add files via upload

---
+++ decimalToBinary.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int decimalToBinary(int n)
+{
+    if (n <= 0)
+        return 0;
+    else
+    {
+        cout << n % 2;
+        return decimalToBinary(n / 2);
+    }
+}
+
+int main()
+{
+    cout << decimalToBinary(2);
+}
